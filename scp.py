bcd0ce70d797d54ef13c2888dd273c6bc3592290
saper <saper@saper.info>
2013-01-19 22:59:53 +0100

    README file

--- scp.py
+++ scp.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
 #
 # Trivial implementation of the scp/rcp
--- test_scp.py
+++ test_scp.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-
+# -*- coding: utf-8 -*-
 #
 # Simple test suite for scp implementation
 #
