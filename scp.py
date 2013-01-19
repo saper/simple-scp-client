7d439a43afb747b49a490369324f437bf804d33c
saper <saper@saper.info>
2013-01-19 01:33:26 +0100

    Initial working version

---
+++ scp.py
@@ -0,0 +1,89 @@
+#! /usr/bin/env python
+import subprocess
+import re
+
+test = [
+    # CannotCopyFileError
+    ("ssh", 22, "saper@zz.saper.info", "scp", "-f", "hooks/wrong"),
+    # RemoteFileNotFoundError
+    ("ssh", 22, "saper@l.saper.info", "scp", "-f", "hooks/wrong"),
+    # CannotCopyFileError
+    ("ssh", 2222, "saper@l.saper.info", "scp", "-f", "hooks/wrong"),
+    # CannotCopyFileError
+    ("ssh", 29418, "wrong@l.saper.info", "scp", "-f", "hooks/wrong"),
+    # CannotCopyFileError
+    ("ssh", 29418, "saper@l.saper.info", "xscp", "-f", "hooks/wrong"),
+    # RemoteFileNotFoundError
+    ("ssh", 29418, "saper@l.saper.info", "scp", "-z", "hooks/wrong"),
+    # RemoteFileNotFoundError
+    ("ssh", 29418, "saper@l.saper.info", "scp", "-f", "hooks/wrong"),
+    # OK
+    ("ssh", 29418, "saper@l.saper.info", "scp", "-f", "hooks/commit-msg")
+]
+
+
+SSH_CLIENTS = [
+    (re.compile("plink", re.I), "-P %s", "%s"),
+    (re.compile("ssh", re.I), "-p %s", "%s")
+]
+
+
+class UnknownSSHClientError(Exception):
+    pass
+
+
+class SecureCopyError(Exception):
+    pass
+
+
+class CannotCopyFileError(SecureCopyError):
+    pass
+
+
+class RemoteFileNotFoundError(SecureCopyError):
+    pass
+
+
+def secure_shell(cmd, port, userhost, *argv):
+    conn = None
+    for (pattern, PORT, USERHOST) in SSH_CLIENTS:
+        if re.match(pattern, cmd):
+            if port:
+                conn = (cmd, PORT % port, USERHOST % userhost) + argv
+            else:
+                conn = (cmd, USERHOST % userhost) + argv
+
+    if conn:
+        p = subprocess.Popen(conn + argv,
+                             stdin=subprocess.PIPE,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
+        return p
+    else:
+        raise UnknownSSHClientError(cmd)
+
+
+def remote_copy(p):
+    try:
+        p.stdin.write("\0")
+        filemode = p.stdout.readline()
+        if filemode:
+            if filemode[0] != 'C':
+                raise RemoteFileNotFoundError(filemode[1:])
+            (mode, length, fname) = filemode.split()
+            p.stdin.write("\0")
+            contents = p.stdout.read(int(length))
+            p.stdin.write("\0")
+        else:
+            raise CannotCopyFileError(p.stderr.readline())
+    except IOError:
+        raise CannotCopyFileError(p.stderr.readline())
+    return contents
+
+
+if __name__ == "__main__":
+    for argv in test:
+        try:
+            print(remote_copy(secure_shell(*argv)))
+        except Exception as e:
+            print(repr(e))
