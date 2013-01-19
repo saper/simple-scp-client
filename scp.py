c5b6b071ca1e3bec20805ac3eaeca3eee23a9189
saper <saper@saper.info>
2013-01-19 22:34:53 +0100

    Prepare for Python3k - use "nul" for zero
    Also wait for the process and close all pipes

--- scp.py
+++ scp.py
@@ -53,18 +53,23 @@ def secure_shell(cmd, port, userhost, *argv):
 
 
 def remote_copy(p):
+    nul = "\0"
     try:
-        p.stdin.write("\0")
+        p.stdin.write(nul)
         filemode = p.stdout.readline()
         if filemode:
             if filemode[0] != 'C':
                 raise RemoteFileNotFoundError(filemode[1:])
             (mode, length, fname) = filemode.split()
-            p.stdin.write("\0")
+            p.stdin.write(nul)
             contents = p.stdout.read(int(length))
-            p.stdin.write("\0")
+            p.stdin.write(nul)
         else:
             raise CannotCopyFileError(p.stderr.readline())
     except IOError:
         raise CannotCopyFileError(p.stderr.readline())
+    p.wait()
+    p.stdin.close()
+    p.stdout.close()
+    p.stderr.close()
     return contents
