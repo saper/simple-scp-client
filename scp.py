ff6d6913e53b22c4479eb8d6c96794dcbd6c23f7
saper <saper@saper.info>
2013-01-19 23:00:26 +0100

    First public release of simple scp client

--- scp.py
+++ scp.py
@@ -1,38 +1,39 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Trivial implementation of the scp/rcp
+# protocol to fetch one file from the remote server
+#
+# Supports different SSH clients, like OpenSSH
+# and Simon Tatham's plink.exe
+#
+# Marcin Cieślak <saper@saper.info> 2013
+#
+
 import subprocess
 import re
 
-test = [
-    # CannotCopyFileError
-    ("ssh", 22, "saper@zz.saper.info", "scp", "-f", "hooks/wrong"),
-    # RemoteFileNotFoundError
-    ("ssh", 22, "saper@l.saper.info", "scp", "-f", "hooks/wrong"),
-    # CannotCopyFileError
-    ("ssh", 2222, "saper@l.saper.info", "scp", "-f", "hooks/wrong"),
-    # CannotCopyFileError
-    ("ssh", 29418, "wrong@l.saper.info", "scp", "-f", "hooks/wrong"),
-    # CannotCopyFileError
-    ("ssh", 29418, "saper@l.saper.info", "xscp", "-f", "hooks/wrong"),
-    # RemoteFileNotFoundError
-    ("ssh", 29418, "saper@l.saper.info", "scp", "-z", "hooks/wrong"),
-    # RemoteFileNotFoundError
-    ("ssh", 29418, "saper@l.saper.info", "scp", "-f", "hooks/wrong"),
-    # OK
-    ("ssh", 29418, "saper@l.saper.info", "scp", "-f", "hooks/commit-msg")
-]
-
 
 SSH_CLIENTS = [
-    (re.compile("plink", re.I), "-P %s", "%s"),
-    (re.compile("ssh", re.I), "-p %s", "%s")
+    (re.compile("plink", re.I),
+        lambda p: ("-P", "%s" % p,),
+        lambda uh: ("%s" % uh,)),
+    (re.compile("ssh", re.I),
+        lambda p: ("-p %s" % p,),
+        lambda uh: ("%s" % uh,))
 ]
 
 
-class UnknownSSHClientError(Exception):
+class SSHError(Exception):
     pass
 
 
-class SecureCopyError(Exception):
+class UnknownSSHClientError(SSHError):
+    pass
+
+
+class SecureCopyError(SSHError):
     pass
 
 
@@ -47,14 +48,14 @@ class RemoteFileNotFoundError(SecureCopyError):
 def secure_shell(cmd, port, userhost, *argv):
     conn = None
     for (pattern, PORT, USERHOST) in SSH_CLIENTS:
-        if re.match(pattern, cmd):
+        if re.search(pattern, cmd):
+            conn = [cmd]
             if port:
-                conn = (cmd, PORT % port, USERHOST % userhost) + argv
-            else:
-                conn = (cmd, USERHOST % userhost) + argv
+                conn += PORT(port)
+            conn += USERHOST(userhost)
 
     if conn:
-        p = subprocess.Popen(conn + argv,
+        p = subprocess.Popen(conn + list(argv),
                              stdin=subprocess.PIPE,
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
@@ -64,26 +65,43 @@ def secure_shell(cmd, port, userhost, *argv):
 
 
 def remote_copy(p):
+    nul = "\0"
     try:
-        p.stdin.write("\0")
+        p.stdin.write(nul)
         filemode = p.stdout.readline()
         if filemode:
             if filemode[0] != 'C':
                 raise RemoteFileNotFoundError(filemode[1:])
             (mode, length, fname) = filemode.split()
-            p.stdin.write("\0")
+            p.stdin.write(nul)
             contents = p.stdout.read(int(length))
-            p.stdin.write("\0")
+            p.stdin.write(nul)
         else:
             raise CannotCopyFileError(p.stderr.readline())
     except IOError:
         raise CannotCopyFileError(p.stderr.readline())
+    p.wait()
+    p.stdin.close()
+    p.stdout.close()
+    p.stderr.close()
     return contents
 
 
-if __name__ == "__main__":
-    for argv in test:
-        try:
-            print(remote_copy(secure_shell(*argv)))
-        except Exception as e:
-            print(repr(e))
+if __name__ == '__main__':
+    import sys
+    import os
+    from getopt import getopt
+
+    (opts, args) = getopt(sys.argv[1:], "p:")
+    port = None
+    for (opt, val) in opts:
+        if opt == '-p':
+            port = val
+
+    if len(args) < 1:
+        sys.stderr.write("usage: %s [-p port] user@hostname\n" % sys.argv[0])
+        sys.exit(1)
+
+    sys.stdout.write(remote_copy(secure_shell(os.getenv("GIT_SSH") or "ssh",
+                                 port, args[0], "scp", "-f",
+                                 "hooks/commit-msg")))
---
+++ test_scp.py
@@ -0,0 +1,105 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Simple test suite for scp implementation
+#
+# Marcin Cieślak <saper@saper.info> 2013
+import unittest
+import re
+
+from scp import secure_shell, remote_copy,\
+    CannotCopyFileError, RemoteFileNotFoundError
+
+#
+# List of SSH clients to run the test suite against
+# Make sure proper authentication is available (for example,
+# ssh-agent or pageant)
+#
+#TEST_CLIENTS = [ "ssh", "plink", "C:\\Program Files\\Git\\Bin\\ssh.exe" ]
+TEST_CLIENTS = ["ssh"]
+# Host name that must not resolve
+UNKNOWN_HOST = "zz.saper.info"
+
+# Normal UNIX shell server to connect (not gerrit)
+# This is used to test response to normal
+# (usually OpenSSH) implementation - file not found
+# or directory instead of the file
+SHELL_HOST = "l.saper.info"
+SHELL_SSH_PORT = 22
+SHELL_USERNAME = "saper"
+# The directory below has to exist on SHELL_HOST
+# under SHELL_USERNAME account
+SHELL_DIRECTORY = ".ssh"
+# TCP port that should be closed on SHELL_HOST
+CLOSED_SSH_PORT = 2222
+
+# Gerrit server coordinates
+GERRIT_HOST = "l.saper.info"
+GERRIT_SSH_PORT = 29418
+GERRIT_USERNAME = "saper"
+# Incorrect username on GERRIT_HOST
+WRONG_USERNAME = "wrong"
+
+
+class SimpleSCPClientTestCase(unittest.TestCase):
+
+    def run_scp(self, *argv):
+        argv = tuple((test_client,) + argv)
+        return remote_copy(secure_shell(*argv))
+
+    def testUknownHost(self):
+        self.assertRaises(CannotCopyFileError, self.run_scp,
+                          SHELL_SSH_PORT, UNKNOWN_HOST,
+                          "scp", "-f", "hooks/wrong")
+
+    def testConnectionRefused(self):
+        self.assertRaises(CannotCopyFileError, self.run_scp,
+                          CLOSED_SSH_PORT, UNKNOWN_HOST,
+                          "scp", "-f", "hooks/wrong")
+
+    def testNoSuchFile(self):
+        self.assertRaises(RemoteFileNotFoundError, self.run_scp,
+                          SHELL_SSH_PORT,
+                          "%s@%s" % (SHELL_USERNAME, SHELL_HOST),
+                          "scp", "-f", "hooks/wrong")
+
+    def testDirectoryFile(self):
+        self.assertRaises(RemoteFileNotFoundError, self.run_scp,
+                          SHELL_SSH_PORT,
+                          "%s@%s" % (SHELL_USERNAME, SHELL_HOST),
+                          "scp", "-f", SHELL_DIRECTORY)
+
+    def testWrongUserName(self):
+        self.assertRaises(CannotCopyFileError, self.run_scp,
+                          GERRIT_SSH_PORT,
+                          "%s@%s" % (WRONG_USERNAME, GERRIT_HOST),
+                          "scp", "-f", "hooks/wrong")
+
+    def testWrongCommand(self):
+        self.assertRaises(CannotCopyFileError, self.run_scp,
+                          GERRIT_SSH_PORT,
+                          "%s@%s" % (GERRIT_USERNAME, GERRIT_HOST),
+                          "xscp", "-f", "hooks/wrong")
+
+    def testIllegalArgument(self):
+        self.assertRaises(RemoteFileNotFoundError, self.run_scp,
+                          GERRIT_SSH_PORT,
+                          "%s@%s" % (GERRIT_USERNAME, GERRIT_HOST),
+                          "scp", "-z", "hooks/wrong")
+
+    def testWrongFileName(self):
+        self.assertRaises(RemoteFileNotFoundError, self.run_scp,
+                          GERRIT_SSH_PORT,
+                          "%s@%s" % (GERRIT_USERNAME, GERRIT_HOST),
+                          "scp", "-z", "hooks/wrong")
+
+    def testGitHook(self):
+        f = self.run_scp(GERRIT_SSH_PORT, "%s@%s" %
+                         (GERRIT_USERNAME, GERRIT_HOST),
+                         "scp", "-f", "hooks/commit-msg")
+        SH = re.compile(".*/bin/sh.*")
+        self.assert_(SH.match(f.split()[0]))
+
+if __name__ == "__main__":
+    for test_client in TEST_CLIENTS:
+        unittest.main()
